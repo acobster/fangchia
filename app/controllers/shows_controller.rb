0110a856850c9282f67d0af63d0b14ab11a1d403
The Ambassador of Funk <acobster@gmail.com>
2017-06-29 22:38:50 -0700

    simple pages/shows controller specs

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,58 +1,11 @@
 class ShowsController < ApplicationController
-  before_action :set_show, only: [:show, :edit, :update, :destroy]
-
   # GET /shows
   def index
     @shows = Show.all
   end
 
-  # GET /shows/1
+  # GET /shows/some-show
   def show
+    @show = Show.find_by_slug(params[:slug])
   end
-
-  # GET /shows/new
-  def new
-    @show = Show.new
-  end
-
-  # GET /shows/1/edit
-  def edit
-  end
-
-  # POST /shows
-  def create
-    @show = Show.new(show_params)
-
-    if @show.save
-      redirect_to @show, notice: 'Show was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /shows/1
-  def update
-    if @show.update(show_params)
-      redirect_to @show, notice: 'Show was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /shows/1
-  def destroy
-    @show.destroy
-    redirect_to shows_url, notice: 'Show was successfully destroyed.'
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_show
-      @show = Show.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def show_params
-      params.require(:show).permit(:title, :slug, :description, :datetime, :venue_name, :venue_url, :venue_address, :cover, :cover_note)
-    end
 end
--- spec/controllers/shows_controller_spec.rb
+++ spec/controllers/shows_controller_spec.rb
@@ -29,22 +29,14 @@ RSpec.describe ShowsController, type: :controller do
   # Show. As you add validations to Show, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {slug: 'my-show',
+     title: 'My Show',
+     datetime: '2017-04-01'}
   }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # ShowsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
 
   describe "GET #index" do
     it "returns a success response" do
-      show = Show.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      get :index, params: {}
       expect(response).to be_success
     end
   end
@@ -52,89 +44,8 @@ RSpec.describe ShowsController, type: :controller do
   describe "GET #show" do
     it "returns a success response" do
       show = Show.create! valid_attributes
-      get :show, params: {id: show.to_param}, session: valid_session
+      get :show, params: {slug: show.slug}
       expect(response).to be_success
-    end
-  end
-
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns a success response" do
-      show = Show.create! valid_attributes
-      get :edit, params: {id: show.to_param}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Show" do
-        expect {
-          post :create, params: {show: valid_attributes}, session: valid_session
-        }.to change(Show, :count).by(1)
-      end
-
-      it "redirects to the created show" do
-        post :create, params: {show: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(Show.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {show: invalid_attributes}, session: valid_session
-        expect(response).to be_success
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested show" do
-        show = Show.create! valid_attributes
-        put :update, params: {id: show.to_param, show: new_attributes}, session: valid_session
-        show.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "redirects to the show" do
-        show = Show.create! valid_attributes
-        put :update, params: {id: show.to_param, show: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(show)
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
-        show = Show.create! valid_attributes
-        put :update, params: {id: show.to_param, show: invalid_attributes}, session: valid_session
-        expect(response).to be_success
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested show" do
-      show = Show.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: show.to_param}, session: valid_session
-      }.to change(Show, :count).by(-1)
-    end
-
-    it "redirects to the shows list" do
-      show = Show.create! valid_attributes
-      delete :destroy, params: {id: show.to_param}, session: valid_session
-      expect(response).to redirect_to(shows_url)
     end
   end
 
