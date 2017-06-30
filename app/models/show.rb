bca0759129dc1823ed5a6456820513223c782de1
The Ambassador of Funk <acobster@gmail.com>
2017-06-29 23:19:01 -0700

    Show model spec

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,9 @@
 class Show < ApplicationRecord
+  validates_presence_of :datetime
+  validates_presence_of :slug
+  validates_uniqueness_of :slug
+  validates_presence_of :title
+
+  scope :upcoming, -> { where('datetime > ?', Time.now) }
+  scope :past, -> { where('datetime < ?', Time.now) }
 end
--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Show, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!('past_show') do
+    Show.create!({slug: 'past-show',
+                  title: 'Show in the Past',
+                  datetime: '2017-01-01'})
+  end
+
+  let!('upcoming_show') do
+    Show.create!({slug: 'upcoming-show',
+                  title: 'Far Future Cyborg Fang Chia',
+                  datetime: '2525-12-31'})
+  end
+
+  it 'gets upcoming shows' do
+    expect(Show.upcoming).to eq([upcoming_show])
+  end
+
+  it 'gets past shows' do
+    expect(Show.past).to eq([past_show])
+  end
 end
