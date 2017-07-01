f415c0e0560f1248acb3a6a7c7ec0110e474466d
The Ambassador of Funk <acobster@gmail.com>
2017-07-01 15:20:55 -0700

    use of a layout is universally predicated on presence of `ajax` param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout :get_layout
+
+
+  private
+
+  # Get the appropriate layout for the current request
+  def get_layout
+    # don't use a layout at all when fulfilling AJAX requests,
+    # otherwise use the normal app layout
+    params[:ajax].present? ? false : 'application'
+  end
 end
