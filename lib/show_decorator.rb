78a59b1b5f9209792fa4c04de2e7a3cd33f1611d
Coby Tamayo <ctamayo@sitecrafting.com>
2017-07-08 11:56:01 -0700

    lots of ShowDecorator stuff

--- lib/show_decorator.rb
+++ lib/show_decorator.rb
@@ -12,9 +12,30 @@ class ShowDecorator
 
   def display_cover()
     return cover_note if cover_note.present?
+    return '' if cover.nil?
 
     # display whole-dollar prices without decimals
     format = (cover.round == cover) ? '$%d' : '$%.2f'
     sprintf(format, cover)
+  end
+
+  def display_time()
+    datetime.strftime('%l:%M %p')
+  end
+
+  def display_date()
+    datetime.strftime('%B %e')
+  end
+
+  def display_datetime()
+    datetime.strftime('%B %e, %l:%M %p')
+  end
+
+  def display_machine_time()
+    datetime.strftime('%H:%M')
+  end
+
+  def html_title()
+    datetime.strftime('%b %e')+' @ '+venue_name+' | Fang Chia'
   end
 end
--- spec/lib/show_decorator_spec.rb
+++ spec/lib/show_decorator_spec.rb
@@ -1,7 +1,7 @@
 require 'show_decorator'
 
 RSpec.describe ShowDecorator do
-  describe '#cover_note' do
+  describe '#display_cover' do
     it 'displays whole-dollar prices' do
       show = instance_double('Show', cover: 5.0, cover_note: '')
       decorator = ShowDecorator.new(show)
@@ -18,6 +18,50 @@ RSpec.describe ShowDecorator do
       show = instance_double('Show', cover: 3.0, cover_note: '$5 suggested')
       decorator = ShowDecorator.new(show)
       expect(decorator.display_cover).to eq('$5 suggested')
+    end
+
+    it 'returns the empty string when cover and cover_note are nil' do
+      show = instance_double('Show', cover: nil, cover_note: nil)
+      decorator = ShowDecorator.new(show)
+      expect(decorator.display_cover).to eq('')
+    end
+  end
+
+  context 'displaying date/time' do
+    # Show on April 20th, 7:30pm
+    let(:decorator) do
+      date = DateTime.new(2017, 4, 20, 19, 30)
+      ShowDecorator.new(instance_double('Show', datetime: date, venue_name: 'Real Art'))
+    end
+
+    describe '#display_date' do
+      it 'formats the date' do
+        expect(decorator.display_date()).to eq('April 20')
+      end
+    end
+
+    describe '#display_time' do
+      it 'formats the time' do
+        expect(decorator.display_time()).to eq(' 7:30 PM')
+      end
+    end
+
+    describe '#display_datetime' do
+      it 'formats the time' do
+        expect(decorator.display_datetime()).to eq('April 20,  7:30 PM')
+      end
+    end
+
+    describe '#display_machine_time' do
+      it 'formats the time for an HTML5 datetime attribute' do
+        expect(decorator.display_machine_time()).to eq('19:30')
+      end
+    end
+
+    describe '#html_title' do
+      it 'formats the date and venue name' do
+        expect(decorator.html_title()).to eq('Apr 20 @ Real Art | Fang Chia')
+      end
     end
   end
 end
