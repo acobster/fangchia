0125cf93ea20148d639dace94a26524c3665c63f
Coby Tamayo <ctamayo@sitecrafting.com>
2017-07-08 11:57:14 -0700

    require venue_name

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,7 @@ class Show < ApplicationRecord
   validates_presence_of :slug
   validates_uniqueness_of :slug
   validates_presence_of :title
+  validates_presence_of :venue_name
 
   scope :upcoming, -> { where('datetime > ?', Time.now) }
   scope :past, -> { where('datetime < ?', Time.now) }
--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Show, type: :model do
   let!('past_show') do
-    Show.create!({slug: 'past-show',
+    Show.create!({datetime: '2017-01-01',
+                  slug: 'past-show',
                   title: 'Show in the Past',
-                  datetime: '2017-01-01'})
+                  venue_name: 'Wherever'})
   end
 
   let!('upcoming_show') do
-    Show.create!({slug: 'upcoming-show',
+    Show.create!({datetime: '2525-12-31',
+                  slug: 'upcoming-show',
                   title: 'Far Future Cyborg Fang Chia',
-                  datetime: '2525-12-31'})
+                  venue_name: 'Wherever'})
   end
 
   it 'gets upcoming shows' do
